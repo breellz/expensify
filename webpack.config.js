f44e2ed9dcd5ea8e226be57a9d03681c637e4b5b
Bassit <barseetbrown@gmail.com>
2020-04-18 15:27:14 +0100

    set up production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
-//const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 module.exports = (env) => {
   const isProduction = env === 'production'
- // const CSSExtract = new ExtractTextPlugin("style.css");
+  const CSSExtract = new ExtractTextPlugin("style.css");
 
   console.log('env', env)
   return {
@@ -19,17 +19,27 @@ module.exports = (env) => {
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use:[
-            'style-loader', 
-            'css-loader',
-            'sass-loader'
-        ]
+        use: CSSExtract.extract({
+          use:[
+            {
+              loader:'css-loader',
+              options:{
+                sourceMap:true
+              }
+            },{
+              loader:'sass-loader',
+              options:{
+                sourceMap:true
+              }
+            }
+          ]
+        })
       }]
     },
-   // plugins:[
-   //   CSSExtract
-   // ],
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    plugins:[
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
