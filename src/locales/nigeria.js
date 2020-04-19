fafd60a37f3eae9547a6dd0bd4930c047a2a3d74
Bassit <barseetbrown@gmail.com>
2020-04-19 10:44:21 +0100

    setup formatting for amount and createdat

---
+++ src/locales/nigeria.js
@@ -0,0 +1,23 @@
+import numeral from 'numeral'
+// load a locale
+numeral.register('locale', 'fr', {
+    delimiters: {
+        thousands: ' ',
+        decimal: '.'
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal : function (number) {
+        return number === 1 ? 'er' : 'ème';
+    },
+    currency: {
+        symbol: '₦'
+    }
+  });
+  
+  // switch between locales
+  numeral.locale('fr');
