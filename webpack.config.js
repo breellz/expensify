8c5f77c0fe163e0e8286bb7adf017460e1421ec8
Bassit <barseetbrown@gmail.com>
2020-05-06 17:44:06 +0100

    Add expensify style

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = (env) => {
 
   console.log('env', env)
   return {
-    entry: './src/app.js',
+    entry: ['babel-polyfill','./src/app.js'],
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
