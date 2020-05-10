7a08b325073fb0c3a33d811c8927e45d7c13cc06
Bassit <barseetbrown@gmail.com>
2020-05-10 13:58:38 +0100

    fixed reload issue

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -8,7 +8,7 @@ export const ExpenseList = (props) => (
     <div className="list-header">
         <div className="show-for-mobile" >Expenses</div>
         <div className="show-for-desktop" >Expense</div>
-        <div className="show-for-ddesktop" >Amount</div>
+        <div className="show-for-desktop" >Amount</div>
     </div>
     <div className="list-body">
     {
